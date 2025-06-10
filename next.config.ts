bb4472ac93fc8095a8aca6b1a05cd671b5e7f4c6
Tejas Chavan <64842361+TejasJChavan@users.noreply.github.com>
2025-06-10 06:33:11 +0530

    Chapter 11 Complete

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  devIndicators:false
+    /* config options here */
+    experimental: {
+        ppr: "incremental",
+    },
+    devIndicators: false,
 };
 
 export default nextConfig;
